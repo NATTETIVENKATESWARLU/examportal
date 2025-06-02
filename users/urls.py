7b8b41ae26322c18301beb5f650d910e2e627305
NATTETIVENKATESWARLU <n.venkateswarlu180@gmail.com>
2025-06-02 17:51:08 +0530

    logouturl update

--- users/urls.py
+++ users/urls.py
@@ -23,12 +23,13 @@ urlpatterns = [
     path('profile/update/', UpdateBasicProfileView.as_view(), name='update_basic_profile'),
     path('profile/details/', UpdateProfileDetailsView.as_view(), name='update_profile_details'),
     path('address/', UpdateAddressView.as_view(), name='update_address'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     # New Password Management URLs
     path('auth/password/change/', ChangePasswordView.as_view(), name='change_password'),
     path('auth/password/reset/request/', RequestPasswordResetView.as_view(), name='request_password_reset_otp'),
     path('auth/password/reset/verify-otp/', VerifyOTPView.as_view(), name='verify_otp_password_reset'),
     path('auth/password/reset/confirm/', SetNewPasswordView.as_view(), name='set_new_password_after_otp'),# Renamed 'set-new' to 'confirm' for clarity
-    path('auth/logout/', LogoutView.as_view(), name='logout'),
+    
 
 
 ]
